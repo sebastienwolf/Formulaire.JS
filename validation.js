ffc87b8422372d4119d58540121338d3ead8e204
Sébastien mouret <s.mouret.pro@gmail.com>
2022-01-12 14:33:57 +0100

    nivea2 fait sans css

--- validation.js
+++ validation.js
@@ -1,9 +1,26 @@
-document.getElementById("validation").addEventListener("submit", function (vérif) {
+function ageVerif() {
 
-  var age = document.getElementById('age').value
-  if (age < 18) {
-    window.alert("vous n'êtes pas majeur");
-  } else if (age >= 18) {
-    window.alert("vous pouvez voter")
+  var age = document.getElementById("ageDate").value;
+  console.log("age =" + age);
+
+  var mSeconde = Date.parse(age);
+  console.log("mSeconde=" + mSeconde);
+
+  const today = new Date();
+  console.log("today = " + today);
+
+  var todayM = today.setHours(0, 0, 0);
+  console.log("todayM = " + todayM);
+
+  var dif = today - mSeconde;
+  console.log("dif =" + dif);
+
+  var difAnnée = dif / 31556952000;
+  console.log("difAnnée =" + difAnnée);
+
+  if (difAnnée < 18) {
+    window.alert("vous n'êtes pas majeur!")
+  } else if (difAnnée >= 18) {
+    window.alert("vous êtes majeur!")
   }
-})
+}
